39711124f1fdd02458e9020099637771e1b69990
Kaiee Brown <88071359+Moon1425@users.noreply.github.com>
2021-12-20 03:38:06 -0500

    Create Kaiee Brown.py

---
+++ Kaiee Brown.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+var = "kaybrown"
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import time
+
+
+
+st.title("The Sky is The Limit")
+
+Bar Charts of the costs 
+
+costs = inpatient_ny.groupby('provider_name')['avsterage_total_payments'].sum().reset_index()
+costs['average_total_payments'] = costs['average_total_payments'].astype('int64')
+
+
+costs_medicare = inpatient_ny.groupby('provider_name')['average_medicare_payments'].sum().reset_index()
+costs_medicare['average_medicare_payments'] = costs_medicare['average_medicare_payments'].astype('int64')
+
+
+costs_sum = costs.merge(costs_medicare, how='left', left_on='provider_name', right_on='provider_name')
+costs_sum['delta'] = costs_sum['average_total_payments'] - costs_sum['average_medicare_payments']
+
+
+st.title('COSTS')
+
+bar3 = px.bar(costs_sum, x='provider_name', y='average_total_payments')
+st.plotly_chart(bar3)
+st.header("Hospital - ")
+st.dataframe(costs_sum)
+
+
+#Costs by Condition and Hospital / Average Total Payments
+costs_condition_hospital = inpatient_ny.groupby(['provider_name', 'drg_definition'])['average_total_payments'].sum().reset_index()
+st.header("Costs by Condition and Hospital - Average Total Payments")
+st.dataframe(costs_condition_hospital)
+
+
+
+# hospitals = costs_condition_hospital['provider_name'].drop_duplicates()
+# hospital_choice = st.sidebar.selectbox('Select your hospital:', hospitals)
+# filtered = costs_sum["provider_name"].loc[costs_sum["provider_name"] == hospital_choice]
+# st.dataframe(filtered)
+
+
